df808554bb6d45cc0bdd1e21b66238a2e8e7d9e9
Alessandro Chitolina <alekitto@gmail.com>
2019-02-22 01:31:06 +0100

    some other console typings

---
+++ src/Component/Console/types/Input/InputAwareInterface.d.ts
@@ -0,0 +1,8 @@
+declare namespace Jymfony.Component.Console.Input {
+    export class InputAwareInterface {
+        /**
+         * Sets the Console Input.
+         */
+        public /* writeonly */ input: InputInterface;
+    }
+}
